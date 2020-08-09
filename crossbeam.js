d100b332099e464dc3b294be5fb1dcdecbc60688
Annie Liao <liaowow@gmail.com>
2020-08-09 11:53:46 -0400

    added recursive approach

--- crossbeam.js
+++ crossbeam.js
@@ -7,7 +7,7 @@ returns an ordered list of all the tasks to run.
 null)
 */
 
-const tasks = [ 
+let tasks = [ 
   {
   task: "make a sandwich",
   depends: [ "buy groceries" ]
@@ -22,22 +22,57 @@ const tasks = [
   } 
 ]
 
-function determineOrder(tasks, subset) {
-  let result = []
-  for (let task of tasks) {
-    if (task.depends.length === 1) {
-      result.unshift(task.depends[0])
-    }
+// test case where tasks are not in order
+tasks = [ 
+  {
+  task: "wear a mask",
+  depends: []
+  }, 
+  {
+  task: "make a sandwich",
+  depends: [ "buy groceries" ]
+  }, 
+  {
+  task: "eat a sandwich",
+  depends: [ "make a sandwich" ]
+  },
+  {
+  task: "buy groceries",
+  depends: [ "go to the store" ]
+  }, 
+  {
+  task: "go to the store",
+  depends: [ "wear a mask" ]
   }
-  for (let sub of subset) {
-    result.push(sub)
+]
+
+function determineOrder(tasks, subset) {
+  let orderedTasks = []
+
+  for (let subTask of subset) {
+    let foundTask = tasks.find(taskObj => taskObj.task === subTask)
+    getDependedTasks(foundTask, orderedTasks, tasks)
   }
 
-  return [...new Set(result)]
+  return [...new Set(orderedTasks)]
+}
+
+// helper function
+function getDependedTasks(currentTask, orderedTasks, tasks) {
+  orderedTasks.unshift(currentTask.task)
+  if (currentTask.depends.length === 0) {
+    return
+  }
+
+  let nextTask = tasks.find(taskObj => taskObj.task === currentTask.depends[0])
+  return getDependedTasks(nextTask, orderedTasks, tasks)
 }
 
 determineOrder(tasks, ["make a sandwich"])
-// -> [ 'go to the store', 'buy groceries', 'make a sandwich' ]
+// -> [ 'wear a mask', 'go to the store', 'buy groceries', 'make a sandwich' ]
 
 determineOrder(tasks, ["buy groceries", "make a sandwich"])
-// -> [ 'go to the store', 'buy groceries', 'make a sandwich' ]
+// -> [ 'wear a mask', 'go to the store', 'buy groceries', 'make a sandwich' ]
+
+determineOrder(tasks, ["go to the store"])
+// -> [ 'wear a mask', 'go to the store' ]
